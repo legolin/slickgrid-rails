7a69e30e0df1faa948f7dcbc193c02da7ca88931
Andrew Sodt <AndrewS@alleninstitute.org>
2014-01-15 11:19:05 -0800

    rails 4 upgrade

--- vendor/assets/javascripts/slick.js
+++ vendor/assets/javascripts/slick.js
@@ -1,6 +1,6 @@
 //= require jquery
-//= require jquery.ui.all
-//= require jquery/event/drag
-//= require jquery/event/drop
+//= require jquery-migrate-1.0.0
+//= require jquery.event.drag-2.2
+//= require jquery-ui-1.8.16.custom.min
 //= require slick/core
 //= require slick/grid
